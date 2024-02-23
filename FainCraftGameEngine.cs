41be3681f661c631ffb6083a244b938fa81cff8a
TheDustySheep <43333779+TheDustySheep@users.noreply.github.com>
2024-02-23 18:36:31 +1100

    Revert "Refactored Texutre2D array loading."
    This reverts commit e400ea198d62bc18583390fa94edc8aa400bc1df.

--- FainCraftGameEngine.cs
+++ FainCraftGameEngine.cs
@@ -6,14 +6,14 @@ using FainEngine_v2.UI;
 namespace FainCraft;
 internal class FainCraftGameEngine : FainGameEngine
 {
-    public FainCraftGameEngine() : base(1600, 900, windowTitle: "FainCraft")
+    public FainCraftGameEngine() : base(1600*2, 900*2, windowTitle: "FainCraft")
     {
     }
 
     protected override void Load()
     {
         //EntityManager.SpawnEntity<SystemDiagnostics>();
-        //var ui = EntityManager.SpawnEntity<UICanvas>();
+        var ui = EntityManager.SpawnEntity<UICanvas>();
         var world = EntityManager.SpawnEntity<World>();
         var player = EntityManager.SpawnEntity(new PlayerEntity(world));
     }
