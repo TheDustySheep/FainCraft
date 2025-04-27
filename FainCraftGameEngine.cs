3accfa968df6ac822ca446d6d6f617f698cb589f
TheDustySheep <43333779+TheDustySheep@users.noreply.github.com>
2025-04-27 22:25:06 +1000

    Added basic UI Layout to handle boxes, sizes, padding, positioning, fitting children.

--- FainCraftGameEngine.cs
+++ FainCraftGameEngine.cs
@@ -2,6 +2,7 @@
 using FainCraft.Gameplay.WorldScripts;
 using FainCraft.Gameplay.WorldScripts.Systems;
 using FainEngine_v2.Core;
+using FainEngine_v2.Entities;
 using FainEngine_v2.Rendering.PostProcessing;
 using FainEngine_v2.Resources;
 
@@ -16,8 +17,11 @@ internal class FainCraftGameEngine : FainGameEngine
     {
         EntityManager.SpawnEntity<SystemDiagnostics>();
 
-        var post_shader = ResourceLoader.LoadShader(@"C:\Users\Sean\source\repos\FainCraft\Resources\Shaders\PostProcessing\");
-        var ui = EntityManager.SpawnEntity(new PostProcess(post_shader));
+        var postShader  = ResourceLoader.LoadShader(@"C:\Users\Sean\source\repos\FainCraft\Resources\Shaders\PostProcessing\");
+        var postProcess = EntityManager.SpawnEntity(new PostProcess(postShader));
+
+        var ui = EntityManager.SpawnEntity<UIController>(); 
+
         var world = EntityManager.SpawnEntity<World>();
         var player = EntityManager.SpawnEntity(new PlayerEntity(world));
     }
